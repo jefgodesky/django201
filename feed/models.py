37d273b4672a8bc8c67eb9b07ada273e4a0ff4fc
Jason Godesky <jefgodesky@gmail.com>
2023-09-06 14:23:28 -0400

    Set up homepage

--- feed/models.py
+++ feed/models.py
@@ -4,6 +4,6 @@ from django.db import models
 class Post(models.Model):
     text = models.CharField(max_length=240)
     date = models.DateTimeField(auto_now=True)
-
+    objects = models.Manager()
     def __str__(self):
         return self.text
