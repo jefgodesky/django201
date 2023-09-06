e66110cd0da5e5594b4fa3c5f467d3ebd2fda42d
Jason Godesky <jefgodesky@gmail.com>
2023-09-06 15:46:23 -0400

    Create homepage template
    y

--- feed/views.py
+++ feed/views.py
@@ -5,7 +5,7 @@ from .models import Post
 
 class Home(ListView):
     http_method_names = ['get']
-    template_name = 'index.html'
+    template_name = 'feed/index.html'
     model = Post
     context_object_name = 'posts'
     queryset = Post.objects.all().order_by('-id')[0:30]
