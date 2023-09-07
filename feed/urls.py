bd03cd46b413c8de7f9363e91a5b316d9ecf03e8
Jason Godesky <jefgodesky@gmail.com>
2023-09-07 11:01:09 -0400

    Add permalinks for each post

--- feed/urls.py
+++ feed/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'feed'
 
 urlpatterns = [
-    path('', views.Home.as_view(), name='index')
+    path('', views.PostFeedView.as_view(), name='index'),
+    path('<int:pk>/', views.PostDetailView.as_view(), name='detail')
 ]
--- feed/views.py
+++ feed/views.py
@@ -1,11 +1,17 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import Post
 
 
-class Home(ListView):
+class PostFeedView(ListView):
     http_method_names = ['get']
     template_name = 'feed/index.html'
     model = Post
     context_object_name = 'posts'
     queryset = Post.objects.all().order_by('-id')[0:30]
+
+class PostDetailView(DetailView):
+    http_method_names = ['get']
+    template_name = 'feed/detail.html'
+    model = Post
+    context_object_name = 'post'
