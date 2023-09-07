91702cd1d201c5bdaa6c2bfff10fc267d969847c
Jason Godesky <jefgodesky@gmail.com>
2023-09-07 15:59:48 -0400

    Rename Followers model to Follower (singular)

--- followers/models.py
+++ followers/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
-class Followers(models.Model):
+class Follower(models.Model):
     followed_by = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
