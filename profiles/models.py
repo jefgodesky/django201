a046d663610da8a9d2eefb71abfb363dd7022b38
Jason Godesky <jefgodesky@gmail.com>
2023-09-07 10:14:15 -0400

    Profiles model: Add image
    Migrate image

--- profiles/models.py
+++ profiles/models.py
@@ -3,6 +3,8 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+from sorl.thumbnail import ImageField
+
 
 class Profile(models.Model):
     user = models.OneToOneField(
@@ -10,6 +12,7 @@ class Profile(models.Model):
         on_delete=models.CASCADE,
         related_name='profile'
     )
+    image = ImageField(upload_to='profiles')
 
     def __str__(self):
         return self.user.username
