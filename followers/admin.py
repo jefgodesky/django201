dc68d1c9d95c42156888a7e0bba1996df7831736
Jason Godesky <jefgodesky@gmail.com>
2023-09-07 15:59:54 -0400

    Register Follower model to admin

--- followers/admin.py
+++ followers/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Follower
+
+
+class FollowersAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Follower, FollowersAdmin)
