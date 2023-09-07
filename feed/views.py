b87599159dfce2a0d199087e42590e88e1f3abce
Jason Godesky <jefgodesky@gmail.com>
2023-09-07 17:12:39 -0400

    Feed: Show only posts from people I follow

--- feed/views.py
+++ feed/views.py
@@ -1,16 +1,30 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView, DetailView
+from django.views.generic import DetailView, TemplateView
 from django.views.generic.edit import CreateView
 
 from .models import Post
+from followers.models import Follower
 
 
-class PostFeedView(ListView):
+class PostFeedView(TemplateView):
     http_method_names = ['get']
     template_name = 'feed/index.html'
-    model = Post
-    context_object_name = 'posts'
-    queryset = Post.objects.all().order_by('-id')[0:30]
+    request = None
+
+    def dispatch(self, request, *args, **kwargs):
+        self.request = request
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        posts = Post.objects.all().order_by('-id')[0:30]
+        if self.request.user.is_authenticated:
+            following = list(Follower.objects.filter(followed_by=self.request.user).values_list('following', flat=True))
+            if following:
+                posts = Post.objects.filter(author__in=following).order_by('-id')[0:60]
+
+        context['posts'] = posts
+        return context
 
 
 class PostDetailView(DetailView):
