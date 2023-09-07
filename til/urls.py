60f13993e948ac7424501ccb2655dbdf05320ab9
Jason Godesky <jefgodesky@gmail.com>
2023-09-07 10:22:45 -0400

    Set up static media

--- til/urls.py
+++ til/urls.py
@@ -14,7 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import include
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, re_path
 
@@ -24,4 +26,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(feed_urls, namespace='feed')),
     re_path('', include('allauth.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
