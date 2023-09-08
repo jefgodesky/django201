6bbd43a0ba13045ca94168f365787283a64e6c57
Jason Godesky <jefgodesky@gmail.com>
2023-09-08 13:22:19 -0400

    Project: Add form to change password
    Add heading

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
 from . import views
@@ -6,6 +7,7 @@ app_name = 'profiles'
 
 urlpatterns = [
     path('', views.ProfileUpdateView.as_view(), name='update'),
+    path('password/', auth_views.PasswordChangeView.as_view(template_name='profiles/password.html'), name='password'),
     path('<str:username>/', views.ProfileDetailView.as_view(), name='detail'),
     path('<str:username>/follow/', views.ProfileFollowView.as_view(), name='follow')
 ]
--- til/urls.py
+++ til/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls import include
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 from django.contrib import admin
 from django.urls import path, re_path
 
@@ -28,4 +29,5 @@ urlpatterns = [
     path('', include(feed_urls, namespace='feed')),
     path('profile/', include(profiles_urls, namespace='profile')),
     re_path('', include('allauth.urls')),
+    path('profiles/password/done', auth_views.PasswordChangeDoneView.as_view(template_name='profiles/password.done.html'), name='password_change_done'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
